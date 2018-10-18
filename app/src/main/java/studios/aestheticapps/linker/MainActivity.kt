05177a564c691b63d37cc63bec45117b3ceb05e6
smutkiewicz <smutkiewicz@gmail.com>
2018-10-18 12:30:34 +0200

    0-Gradle setup

--- app/src/main/java/studios/aestheticapps/linker/MainActivity.kt
+++ app/src/main/java/studios/aestheticapps/linker/MainActivity.kt
@@ -5,12 +5,10 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
