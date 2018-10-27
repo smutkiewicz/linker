addc775270dfecdbceb0985f853a0a4d347f0ad3
smutkiewicz <smutkiewicz@gmail.com>
2018-10-27 12:04:43 +0200

    2-Splash screen added, appbar background added

--- app/src/main/java/studios/aestheticapps/linker/MainActivity.kt
+++ app/src/main/java/studios/aestheticapps/linker/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
     override fun onResume()
---
+++ app/src/main/java/studios/aestheticapps/linker/SplashActivity.kt
@@ -0,0 +1,22 @@
+package studios.aestheticapps.linker
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        launchMainActivity()
+    }
+
+    private fun launchMainActivity()
+    {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
